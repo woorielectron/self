217d265a935caa3da6aa9c3903b63401cb0bfb44
woorielectron <woorielectron@gmail.com>
2024-12-02 18:51:29 +0900

    dbtest 클라이언트 서버 연결

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,30 @@
-// src/server.ts
+// server.ts
 import express, { Request, Response } from 'express';
+import next from 'next';
 
-const app = express();
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
 const port = 3000;
 
-app.get('/api/beta', (req: Request, res: Response) =>
+app.prepare().then(() =>
 {
-  res.status(200).send('beta')
-});
+  const server = express();
 
-app.listen(port, () =>
-{
-  console.log(`Server is running on http://localhost:${port}`);
+  server.get('/api/hello', (req: Request, res: Response) =>
+  {
+    res.status(200).json({ message: 'Hello from Express!' });
+  });
+
+  server.all('*', (req: Request, res: Response) =>
+  {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err?: Error) =>
+  {
+    if (err) throw err;
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 });
