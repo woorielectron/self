34829dbac836497f71549bc0adfaee043782f50b
woorielectron <woorielectron@gmail.com>
2024-12-16 23:07:31 +0900

    client-monitor

---
+++ src/server/server.ts
@@ -0,0 +1,38 @@
+import http, { IncomingMessage, ServerResponse } from 'http';
+import dotenv from 'dotenv';
+dotenv.config();
+import path from 'path';
+import fs from 'fs';
+import express from 'express';
+
+import { myUser } from './components/user';
+
+const hostname: string = 'localhost';
+const port = Number(process.env.NEXT_PUBLIC_PORT);
+
+const server = express();
+server.use(express.json());
+
+server.listen(port, hostname, () =>
+{
+  console.log('listening');
+});
+server.get('/home/:id', (req, res) =>
+{
+  res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf8' });
+  res.end(`get 전송  ${req.params.id}`);
+});
+server.post('/home/:id', (req, res) =>
+{
+  console.log(req.body);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`post 전송 ${req.params.id}`);
+
+});
+fs.writeFile("./test.txt", "hello", err => { });
+
+
+
+
+
+
